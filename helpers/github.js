0ed7651da80f9a28a425000992dc9a2d458b6c2d
Milad Moulayi <m.moulayi123@gmail.com>
2023-04-24 16:04:08 -0700

    Set up schema and axios get requests, need to update schema for new response format

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,28 @@
 const axios = require('axios');
 const config = require('../config.js');
+const url = require('url');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: 'https://api.github.com/users/' + username,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+  axios(options).then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+
 }
+
+getReposByUsername('MiladMoulayi');
+
 
 module.exports.getReposByUsername = getReposByUsername;
